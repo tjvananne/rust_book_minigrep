ccd774194f823fae58be4b15db4869ed241a6db7
tjvananne <tjvananne@gmail.com>
2022-07-04 09:08:02 -0600

    We can accept and print arguments now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
+
+
+// ==== execution of package ===== 
+
+// cargo run
+// ["target\\debug\\rust_book_minigrep.exe"]
+
+// cargo run my sweet args
+// ["target\\debug\\rust_book_minigrep.exe", "my", "sweet", "args"]
+
